b9bdc3152a4c2691824d2bb4b6eba7792bb14462
nickRuddra <biswasruddra100@gmail.com>
2023-04-24 13:15:33 +0600

    rms - event blade config finish

--- laravel_blade_rms/resources/views/frontend/body/events.blade.php
+++ laravel_blade_rms/resources/views/frontend/body/events.blade.php
@@ -1,115 +1,41 @@
  @php
-    $eventSectionTitle = App\Models\EventSectionTitle::find(1);
-@endphp
+     $eventSectionTitle = App\Models\EventSectionTitle::find(1);
+     $eventSection = App\Models\EventSection::get();
+     
+ @endphp
  <section id="events" class="events">
      <div class="container">
 
          <div class="section-title">
-             <h2>{{$eventSectionTitle->title_first}}<span>  {{$eventSectionTitle->title_first}}</span>  {{$eventSectionTitle->title_middle}}</h2>
+             <h2>{{ $eventSectionTitle->title_first }}<span> {{ $eventSectionTitle->title_first }}</span>
+                 {{ $eventSectionTitle->title_middle }}</h2>
          </div>
 
          <div class="events-slider swiper">
              <div class="swiper-wrapper">
 
-                 <div class="swiper-slide">
-                     <div class="row event-item">
-                         <div class="col-lg-6">
-                             <img src="{{ asset('frontend/assets/img/event-birthday.jpg') }}" class="img-fluid"
-                                 alt="">
-                         </div>
-                         <div class="col-lg-6 pt-4 pt-lg-0 content">
-                             <h3>Birthday Parties</h3>
-                             <div class="price">
-                                 <p><span>$189</span></p>
+                 @foreach ($eventSection as $event)
+                     <div class="swiper-slide">
+                         <div class="row event-item">
+                             <div class="col-lg-6">
+                                 <img src="{{ asset($event->image) }}" class="img-fluid" alt="">
                              </div>
-                             <p class="fst-italic">
-                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                 incididunt ut labore et dolore
-                                 magna aliqua.
-                             </p>
-                             <ul>
-                                 <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea
-                                     commodo consequat.</li>
-                                 <li><i class="bi bi-check-circle"></i> Duis aute irure dolor in reprehenderit in
-                                     voluptate velit.</li>
-                                 <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea
-                                     commodo consequat.</li>
-                             </ul>
-                             <p>
-                                 Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                                 reprehenderit in voluptate
-                                 velit esse cillum dolore eu fugiat nulla pariatur
-                             </p>
-                         </div>
-                     </div>
-                 </div><!-- End testimonial item -->
-
-                 <div class="swiper-slide">
-                     <div class="row event-item">
-                         <div class="col-lg-6">
-                             <img src="{{ asset('frontend/assets/img/event-private.jpg') }}" class="img-fluid"
-                                 alt="">
-                         </div>
-                         <div class="col-lg-6 pt-4 pt-lg-0 content">
-                             <h3>Private Parties</h3>
-                             <div class="price">
-                                 <p><span>$290</span></p>
+                             <div class="col-lg-6 pt-4 pt-lg-0 content">
+                                 <h3>{{ $event->title }}</h3>
+                                 <div class="price">
+                                     <p><span>$ {{ $event->price }}</span></p>
+                                 </div>
+                                 <p class="fst-italic"> {{ $event->description_top }} </p>
+                                 <ul>
+                                     <li><i class="bi bi-check-circle"></i> {{ $event->point_1 }} </li>
+                                     <li><i class="bi bi-check-circle"></i> {{ $event->poind_2 }} </li>
+                                     <li><i class="bi bi-check-circle"></i> {{ $event->point_3 }} </li>
+                                 </ul>
+                                 <p> {{ $event->description_bottom }} </p>
                              </div>
-                             <p class="fst-italic">
-                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                 incididunt ut labore et dolore
-                                 magna aliqua.
-                             </p>
-                             <ul>
-                                 <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea
-                                     commodo consequat.</li>
-                                 <li><i class="bi bi-check-circle"></i> Duis aute irure dolor in reprehenderit in
-                                     voluptate velit.</li>
-                                 <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea
-                                     commodo consequat.</li>
-                             </ul>
-                             <p>
-                                 Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                                 reprehenderit in voluptate
-                                 velit esse cillum dolore eu fugiat nulla pariatur
-                             </p>
                          </div>
-                     </div>
-                 </div><!-- End testimonial item -->
-
-                 <div class="swiper-slide">
-                     <div class="row event-item">
-                         <div class="col-lg-6">
-                             <img src="{{ asset('frontend/assets/img/event-custom.jpg') }}" class="img-fluid"
-                                 alt="">
-                         </div>
-                         <div class="col-lg-6 pt-4 pt-lg-0 content">
-                             <h3>Custom Parties</h3>
-                             <div class="price">
-                                 <p><span>$99</span></p>
-                             </div>
-                             <p class="fst-italic">
-                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                 incididunt ut labore et dolore
-                                 magna aliqua.
-                             </p>
-                             <ul>
-                                 <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea
-                                     commodo consequat.</li>
-                                 <li><i class="bi bi-check-circle"></i> Duis aute irure dolor in reprehenderit in
-                                     voluptate velit.</li>
-                                 <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea
-                                     commodo consequat.</li>
-                             </ul>
-                             <p>
-                                 Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                                 reprehenderit in voluptate
-                                 velit esse cillum dolore eu fugiat nulla pariatur
-                             </p>
-                         </div>
-                     </div>
-                 </div><!-- End testimonial item -->
-
+                     </div><!-- End testimonial item -->
+                 @endforeach
              </div>
              <div class="swiper-pagination"></div>
          </div>
